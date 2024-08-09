79c6fdeac98eb4f27b9fb38f6349b1ba85187629
Subin Lee <ewaterbinn@gmail.com>
2024-08-09 18:27:57 +0900

    feat: theme type 정의

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -1,13 +1,14 @@
 import 'styled-components';
 import { theme } from '../styles/theme';
+import { color } from '@/styles';
 
 type Theme = typeof theme;
 
 declare module 'styled-components' {
   export interface DefaultTheme extends Theme {
-    color: {
-      black: string;
-      blue: string;
-    };
+    color: color;
+    fontFamily: string;
+    fontMain: string;
+    fontPrice: string;
   }
 }
