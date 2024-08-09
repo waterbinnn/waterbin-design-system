ce7961d5247f69a80e92f2936d5de4d4f1ee1da5
Subin Lee <ewaterbinn@gmail.com>
2024-08-09 18:24:29 +0900

    feat: export button, global style

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import * as theme from './styles/theme';
+import * as global from './styles/GlobalStyles';
+
+export { theme, global };
+
+export { default as Button } from './components/Button/Button';
