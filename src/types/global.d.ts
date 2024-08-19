5aab27c2b2e1b13fc182b2b6596e53d6b0ce86ae
Subin Lee <ewaterbinn@gmail.com>
2024-08-19 15:16:29 +0900

    fix: svg를 컴포넌트로 사용하기 위한 타입 명시

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,5 +1,8 @@
 declare module '*.svg' {
   import * as React from 'react';
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+  const src: string;
+  export default src;
 }
